63d70b7946c5092e627c3e38cb8bfc4bb8b50bd7
Victor sette <140134596+victordisette@users.noreply.github.com>
2025-09-20 11:37:57 -0300

    Criação da tela de login
    Criação dos botões

---
+++ front-end/src/login.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import "./Login.css";
+import logo from "./assets/logo.png";
+
+<img src={logo} alt="CBMPE" />
+
+
+
+export default function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <div className="login-page">
+      {/* Box do formulário */}
+      <div className="login-box">
+        <h2>
+          PAINEL DE COLETA E <br />
+          GESTÃO DE OCORRÊNCIAS <br />
+          <span>CBMPE</span>
+        </h2>
+
+        {/* Campo Email/CPF */}
+        <div className="input-group">
+          <span className="icon">👤</span>
+          <input type="text" placeholder="E-mail ou CPF" />
+        </div>
+
+        {/* Campo Senha */}
+        <div className="input-group">
+          <span className="icon">🔒</span>
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Senha"
+          />
+          <span
+            className="icon eye"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? "🙈" : "👁️"}
+          </span>
+        </div>
+
+        <a href="#" className="forgot">
+          Esqueceu sua senha?
+        </a>
+
+        <button className="btn">Entrar</button>
+      </div>
+
+      {/* Logo à direita */}
+      <div className="logo-box">
+        
+
+      </div>
+    </div>
+  );
+}
