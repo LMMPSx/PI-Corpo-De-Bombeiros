16d02ae350b6cf52a745ba13e429a182942cd228
Victor sette <140134596+victordisette@users.noreply.github.com>
2025-09-21 12:58:09 -0300

    Criação da tela de esqueceu a senha
    Deixe respansivel

--- front-end/src/login.jsx
+++ front-end/src/login.jsx
@@ -1,61 +1,60 @@
 import { useState } from "react";
-import "./Login.css";
+import "./login.css"; 
 import logo from "./assets/logo.png";
 import cadeado from "./assets/cadeado.png";
-
-
-<img src={logo.png} alt="CBMPE" />
-
-
-
+import Olho from "./assets/olho.png";
+import icone from "./assets/icone.png";
 
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <div className="login-page">
-      {/* Box do formulário */}
-      <div className="login-box">
-        <h2>
-          PAINEL DE COLETA E <br />
-          GESTÃO DE OCORRÊNCIAS <br />
-          <span>CBMPE</span>
-        </h2>
+    <>
+      <div className="login-page">
+        {/* Box do formulário */}
+        <div className="login-box">
+          <h2>
+            PAINEL DE COLETA E <br />
+            GESTÃO DE OCORRÊNCIAS <br />
+            <span>CBMPE</span>
+          </h2>
 
-        {/* Campo Email/CPF */}
-        <div className="input-group">
-          <span className="icon">👤</span>
-          <input type="text" placeholder="E-mail ou CPF" />
+          {/* Campo Email/CPF */}
+          <div className="input-group">
+            {/* Ícone de usuário */}
+            <img src={icone} alt="Ícone de Usuário" className="icon" />
+            <input type="text" placeholder="E-mail ou CPF" />
+          </div>
+
+          {/* Campo Senha */}
+          <div className="input-group">
+            {/* Ícone de cadeado */}
+            <img src={cadeado} alt="Ícone de Cadeado" className="icon" />
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Senha"
+            />
+            {/* Ícone de olho */}
+            <img
+              src={Olho}
+              alt="Ícone de Olho"
+              className="icon eye"
+              onClick={() => setShowPassword(!showPassword)}
+            />
+          </div>
+
+          <a href="#" className="forgot">
+            Esqueceu sua senha?
+          </a>
+
+          <button className="btn">Entrar</button>
         </div>
 
-        {/* Campo Senha */}
-        <div className="input-group">
-          <img src={cadeado.png} alt="" />
-          <input
-            type={showPassword ? "text" : "password"}
-            placeholder="Senha"
-          />
-          <span
-            className="icon eye"
-            onClick={() => setShowPassword(!showPassword)}
-          >
-            {showPassword ? "🙈" : "👁️"}
-          </span>
-        </div>
-
-        <a href="#" className="forgot">
-          Esqueceu sua senha?
-        </a>
-
-        <button className="btn">Entrar</button>
-      </div>
-
-      {/* Logo à direita */}
-      <div className="logo-box">
+        {/* Logo à direita */}
+        <div className="logo-box">
           <img src={logo} alt="Logo" className="logo" />
+        </div>
       </div>
-
-      
-    </div>
+    </>
   );
 }
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import App from './login.jsx'
+import App from './esqueceu.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
